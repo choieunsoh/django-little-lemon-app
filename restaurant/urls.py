f6701b38ade90d5e043a6e451011816a231ca634
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-07-11 23:44:18 +0700

    ✨ feat: add permission to get all bookings

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,11 @@
 # define URL route for index() view
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:id>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
