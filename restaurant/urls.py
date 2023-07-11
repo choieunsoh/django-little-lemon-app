83995b627fed6cf7b6ed2fd154c5b31fd6da012a
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-07-11 21:44:43 +0700

    ✨ feat: add menu, menu by id api endpoints

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:id>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    ordering_fields = ['price', 'inventory']
+    filterset_fields = ['price', 'inventory']
+    search_fields = ['title']
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    #queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = 'id'
+
+    def get_object(self):
+        id = self.kwargs["id"]
+        return get_object_or_404(Menu, id=id)
