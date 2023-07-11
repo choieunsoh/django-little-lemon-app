d9187d7790c1ad51ff0d6bfcf071880ab4e29037
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-07-11 23:13:42 +0700

    ✨ feat: add booking model-view-set

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -26,3 +28,18 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     def get_object(self):
         id = self.kwargs["id"]
         return get_object_or_404(Menu, id=id)
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        serializer = BookingSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        booking = get_object_or_404(self.queryset, pk=pk)
+        serializer = BookingSerializer(booking)
+        return Response(serializer.data)
